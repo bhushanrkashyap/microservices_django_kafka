c50e1ce73073f67a5e805e318a6960dc5e816e5f
bhushanrkashyap <bhushanrkashyap@gmail.com>
2026-01-29 13:37:56 +0530

    check stock and updation of db

--- myapp/consumer.py
+++ myapp/consumer.py
@@ -1,6 +1,10 @@
 import json
+import sys , os , django
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "order.settings")
+django.setup()
+from myapp.models import Inventory
 from confluent_kafka import Consumer
-
 conf = {
     'bootstrap.servers': 'localhost:9092',
     'group.id': 'orders-service',
@@ -21,6 +25,23 @@ while True:
     if msg.error():
         print("Error:", msg.error())
         continue
+    
 
     data = json.loads(msg.value().decode('utf-8'))
     print("Received:", data)
+    product_name = data.get('product_name')
+    quantity = data.get('quantity')
+    try:
+        x = Inventory.objects.get(product_name=product_name).lower()
+        if x and x.quantity > 0:
+            x.quantity -=quantity
+            x.save()
+            print(f"Inventory updated for {product_name}. New quantity: {x.quantity}")
+        else:
+            print(f"Insufficient inventory for {product_name}")
+    except Inventory.DoesNotExist:
+        print(f"Product {product_name} does not exist in inventory")
+
+
+        
+
